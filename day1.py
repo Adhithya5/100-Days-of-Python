5deb6c2133a8f15c359731fb676624d3362c9002
Adhithya R <114201479+Adhithya5@users.noreply.github.com>
2024-11-11 17:16:27 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,23 @@
+#print function
+print("hello world!\nHello world!")
+#String Concatination
+print("hello"+" "+"Kratos")
+print("hello"+" Kratos")
+#getting input from user
+name=input("What is your name?\n")
+print("Ciao! "+name)
+print("ola! "+input("What is your name?\n"))
+#finding the number of letters in a string
+print(len(name))
+#Using temp variable to switch values
+glass1="milk"
+glass2="juice"
+temp=glass1
+glass1=glass2
+glass2=temp
+print(glass1,glass2)
+#Day 1 project Music Band Name Generator
+print("Welcome to the Band Name Generator")
+city_name=input("What's the name of the city you grew up in?\n")
+pet_name=input("what's your pet's name?\n")
+print("your band name could be "+city_name+" "+pet_name)
